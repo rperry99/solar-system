a2f7b4f0d9e8e15b084b064b9091b572283db125
Russ Perry <rperry1886@gmail.com>
2020-05-04 20:40:36 -0400

    Add MI/KM funcitonality
    When a user clicks on the MI or KM, it will swap back and forth between metric and imperial units

--- app.js
+++ app.js
@@ -28,19 +28,112 @@ $(document).ready(function () {
   $("#neptuneDistance").html(neptuneDistance.toLocaleString("en"));
   $("#plutoDistance").html(plutoDistance.toLocaleString("en"));
 
-  console.log("Pluto Distance", $("#plutoDistance").html());
+  let tempDist = 0; //This is to temporarily hold the distance before setting it back to the planet's disnance.
 
   $(".planet-distance").click(function () {
     let currentUnit = $(this).html();
-    let currentNum = $(this).prev().text();
     if (currentUnit === "MI") {
       $(this).html("KM");
-      //   currentNum.replace(/,/g, "");
-      console.log(parseInt(currentNum));
-      //   $(this).prev().html(calculateKM(currentNum));
+      switch ($(this).prev().attr("id")) {
+        case "mercuryDistance":
+          tempDist = calculateKM(mercuryDistance);
+          mercuryDistance = tempDist;
+          $("#mercuryDistance").html(mercuryDistance.toLocaleString("en"));
+          break;
+        case "venusDistance":
+          tempDist = calculateKM(venusDistance);
+          venusDistance = tempDist;
+          $("#venusDistance").html(venusDistance.toLocaleString("en"));
+          break;
+        case "earthDistance":
+          tempDist = calculateKM(earthDistance);
+          earthDistance = tempDist;
+          $("#earthDistance").html(earthDistance.toLocaleString("en"));
+          break;
+        case "marsDistance":
+          tempDist = calculateKM(marsDistance);
+          marsDistance = tempDist;
+          $("#marsDistance").html(marsDistance.toLocaleString("en"));
+          break;
+        case "jupiterDistance":
+          tempDist = calculateKM(jupiterDistance);
+          jupiterDistance = tempDist;
+          $("#jupiterDistance").html(jupiterDistance.toLocaleString("en"));
+          break;
+        case "saturnDistance":
+          tempDist = calculateKM(saturnDistance);
+          saturnDistance = tempDist;
+          $("#saturnDistance").html(saturnDistance.toLocaleString("en"));
+          break;
+        case "uranusDistance":
+          tempDist = calculateKM(uranusDistance);
+          uranusDistance = tempDist;
+          $("#uranusDistance").html(uranusDistance.toLocaleString("en"));
+          break;
+        case "neptuneDistance":
+          tempDist = calculateKM(neptuneDistance);
+          neptuneDistance = tempDist;
+          $("#neptuneDistance").html(neptuneDistance.toLocaleString("en"));
+          break;
+        case "plutoDistance":
+          tempDist = calculateKM(plutoDistance);
+          plutoDistance = tempDist;
+          $("#plutoDistance").html(plutoDistance.toLocaleString("en"));
+          break;
+        default:
+          break;
+      }
     } else if (currentUnit === "KM") {
       $(this).html("MI");
-      console.log(parseInt(currentNum));
+      switch ($(this).prev().attr("id")) {
+        case "mercuryDistance":
+          tempDist = calculateMI(mercuryDistance);
+          mercuryDistance = tempDist;
+          $("#mercuryDistance").html(mercuryDistance.toLocaleString("en"));
+          break;
+        case "venusDistance":
+          tempDist = calculateMI(venusDistance);
+          venusDistance = tempDist;
+          $("#venusDistance").html(venusDistance.toLocaleString("en"));
+          break;
+        case "earthDistance":
+          tempDist = calculateMI(earthDistance);
+          earthDistance = tempDist;
+          $("#earthDistance").html(earthDistance.toLocaleString("en"));
+          break;
+        case "marsDistance":
+          tempDist = calculateMI(marsDistance);
+          marsDistance = tempDist;
+          $("#marsDistance").html(marsDistance.toLocaleString("en"));
+          break;
+        case "jupiterDistance":
+          tempDist = calculateMI(jupiterDistance);
+          jupiterDistance = tempDist;
+          $("#jupiterDistance").html(jupiterDistance.toLocaleString("en"));
+          break;
+        case "saturnDistance":
+          tempDist = calculateMI(saturnDistance);
+          saturnDistance = tempDist;
+          $("#saturnDistance").html(saturnDistance.toLocaleString("en"));
+          break;
+        case "uranusDistance":
+          tempDist = calculateMI(uranusDistance);
+          uranusDistance = tempDist;
+          $("#uranusDistance").html(uranusDistance.toLocaleString("en"));
+          break;
+        case "neptuneDistance":
+          tempDist = calculateMI(neptuneDistance);
+          neptuneDistance = tempDist;
+          $("#neptuneDistance").html(neptuneDistance.toLocaleString("en"));
+          break;
+        case "plutoDistance":
+          tempDist = calculateMI(plutoDistance);
+          plutoDistance = tempDist;
+          $("#plutoDistance").html(plutoDistance.toLocaleString("en"));
+          break;
+        default:
+          break;
+      }
     }
   });
 });
